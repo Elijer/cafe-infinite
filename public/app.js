3a771f112383093b97b442986363717a1e339f8a
Jah <kennedye2@vcu.edu>
2020-05-21 23:04:34 -0400

    append DOM with db data from snapshot

--- public/app.js
+++ public/app.js
@@ -3,22 +3,31 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const db = firebase.firestore();
 
-    const someUser = db.collection('users').doc('RyjhQwEZhXFq51WNhCCr');
+    const products = db.collection('products').doc('HzTEK6EP0ekwxeSVwx1n');
 
     //console.log(app);
     
-    /*someUser.get()
+    /*products.get()     //get a single product
       .then(doc => {
         const data = doc.data();
-        console.log(data.jose);
+        console.log(data.name);
       })*/
 
-      someUser.onSnapshot(doc => {
 
+      products.onSnapshot(doc => { //get a single product when it changes. also does this when app loads, unfortunately
         const data = doc.data();
-        console.log(data);
+        newElly(data.name);
+        //console.log(data);
+        //document.write(data.name + `<br>`); //adds new line for every change in this item, eggs
       })
 });
+
+function newElly(theText){
+  var node = document.createElement("h1");                 // Create a <li> node
+  node.id = "testies";            //give it an ID
+  node.innerText = theText;
+  document.getElementById("theList").appendChild(node);              // Append the text to <li>
+}
 
 // Auth function
 function googleLogin(){
