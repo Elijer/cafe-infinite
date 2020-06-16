1fe2814a94a5dacedb68ec7effa40b0efb33b4d7
Jah <kennedye2@vcu.edu>
2020-06-16 13:06:18 -0500

    onbarding line fires a js function in app.js, plus express is partially set up in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,75 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
-//initialize an admin app instance from which Cloud Firestore changes can be made
+//Firebase and Firestore
+const functions = require('firebase-functions'); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const admin = require('firebase-admin'); //initialize an admin app instance from which Cloud Firestore changes can be made // The Firebase Admin SDK to access Cloud Firestore.
 admin.initializeApp();
-//A hash generator package I found on NPM
 let db = admin.firestore();
-//let statesRef = db.collection('states');
 
-//const createHash = require('hash-generator');
-/* how to use:
-var hashLength = 8;
-var hash = createHash(8);*/
+//express
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
 
+/*
+// build multiple CRUD interfaces:              CRUD stands for CREATE, READ, UPDATE, and DELETE. This is routing for direct database alterations!! Exciting!
+app.get('/:id', (req, res) => res.send(Widgets.getById(req.params.id)));
+app.post('/', (req, res) => res.send(Widgets.create()));
+app.put('/:id', (req, res) => res.send(Widgets.update(req.params.id, req.body)));
+app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
+app.get('/', (req, res) => res.send(Widgets.list()));
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
+*/
+
+
+/*
+//I've put in my specified URI and commented out state validation for now
+//However, this is stripes syntax, not compatable with Firebase. I don't think.
+app.get("/stripe_return.html", async (req, res) => {
+    const { code, state } = req.query;
+  
+    // Assert the state matches the state you provided in the OAuth link (optional).
+    if(!stateMatches(state)) {
+      return res.status(403).json({ error: 'Incorrect state parameter: ' + state });
+    }
+
+      // Send the authorization code to Stripe's API.
+  stripe.oauth.token({
+    grant_type: 'authorization_code',
+    code
+  }).then(
+    (response) => {
+      var connected_account_id = response.stripe_user_id;
+      saveAccountId(connected_account_id);
+
+      // Render some HTML or redirect to a different page.
+      return res.status(200).json({success: true});
+    },
+    (err) => {
+      if (err.type === 'StripeInvalidGrantError') {
+        return res.status(400).json({error: 'Invalid authorization code: ' + code});
+      } else {
+        return res.status(500).json({error: 'An unknown error occurred.'});
+      }
+    }
+  );
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //This kind of cloud function creates an HTTP endpoint event. Can't make it work yet.
 //Got this function from here: https://firebase.google.com/docs/functions/get-started
 // Take the text parameter passed to this HTTP endpoint and insert it into 
@@ -40,3 +97,4 @@ exports.addState = db.collection('states').add({
     }).then(ref => {
         console.log("added a document with ", ref.id);
 });
+*/
