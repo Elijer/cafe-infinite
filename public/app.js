e865aafecb7c2712af41d00ad2fa2ed548cf5932
Jah <kennedye2@vcu.edu>
2020-06-29 10:21:47 -0500

    added extra conditionals to make sure that onboardBusiness and anonLogin run as they should

--- public/app.js
+++ public/app.js
@@ -12,9 +12,10 @@ document.addEventListener("DOMContentLoaded", event => {
     });
     // stripe test keys could go here, while production keys could be kept in a different condition
   }
-
   var stripe = Stripe('pk_test_FjTxRNal2FWcwhlqw0WtIETQ00ZDxO3D9S');  
   document.getElementById("banner-login").innerText = "login";
+
+  // checkComplete bool added to firebase object so that checkForUserPersistence() is only called once
   firebase.checkComplete = false;
   firebase.auth().onAuthStateChanged(user => checkForUserPersistence());
 });
@@ -45,6 +46,7 @@ function checkForUserPersistence(){
   }
 }
 
+// Called when user logs in: formats login button to say their ID
 function loginFormat(id){
   document.getElementById("business-login").style.display = 'inline';
   document.getElementById("banner-login").style.fontSize = '30px';
@@ -53,6 +55,7 @@ function loginFormat(id){
   document.getElementById("business-logout").style.display = 'inline';
 }
 
+// Formats logout AND actually logs user out
 function logOut(){ // more on logging out: https://stackoverflow.com/questions/37343309/best-way-to-implement-logout-in-firebase-v3-0-1-firebase-unauth-is-removed-aft
   document.getElementById("business-logout").style.display = 'none';
   document.getElementById("banner-login").innerText = "login";
@@ -66,48 +69,47 @@ function logOut(){ // more on logging out: https://stackoverflow.com/questions/3
 }
 
 function anonLogin(){
-  /* should check to see if user data has been created in the database
   if (firebase.auth().currentUser){
     var user = firebase.auth().currentUser;
     console.log("User already exists in browser. UID: " + user.uid);
+    return;
   } else {
-  }*/
-
-  firebase.auth().signInAnonymously().catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("There is an error with the login of code: " + errorCode + " and message: " + errorMessage);
-  })
-  .then( result => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        //console.log("User is: " + uid + " .Is anonymous? " + isAnonymous);
-  
-        loginFormat(user.uid);
-  
-        const db = firebase.firestore();
-        const usersRef = db.collection('businesses').doc(uid);
-  
-        usersRef.get()
-          .then((docSnapshot) => {
-            if (!docSnapshot.exists) {
-              let data = {
-                isAnonymous: isAnonymous,
-                createdAt: new Date()
-              };
-              usersRef.set(data, {merge: true}) // create the document
-              console.log("new anon user created in db for UID:  " + user.uid);
-            }
-          })
-        .catch(console.log);
-      } else {
-        console.log("user is not signed in");
-      }
-    });
-  })
+    firebase.auth().signInAnonymously().catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("There is an error with the login of code: " + errorCode + " and message: " + errorMessage);
+    })
+    .then( result => {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          //console.log("User is: " + uid + " .Is anonymous? " + isAnonymous);
+    
+          loginFormat(user.uid);
+    
+          const db = firebase.firestore();
+          const usersRef = db.collection('businesses').doc(uid);
+    
+          usersRef.get()
+            .then((docSnapshot) => {
+              if (!docSnapshot.exists) {
+                let data = {
+                  isAnonymous: isAnonymous,
+                  createdAt: new Date()
+                };
+                usersRef.set(data, {merge: true}) // create the document
+                console.log("new anon user created in db for UID:  " + user.uid);
+              }
+            })
+          .catch(console.log);
+        } else {
+          console.log("user is not signed in");
+        }
+      });
+    })
+  }
 }
 
 function onboardBusiness(){
@@ -122,24 +124,24 @@ function onboardBusiness(){
     console.log("the user is " + user.displayName);
   }
 
-  //check to also see if there's an actual User Account, too
-
   //check to see if the account already has business ID
       const db = firebase.firestore(); //will this fuck up the locahost thing?
       const docRef = db.collection('businesses').doc(user.uid);
       docRef.get().then(function(doc) {
-        if (doc.data().stripeBusinessID) {
-          console.log("There is a Biz ID already", doc.data().stripeBusinessID)
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No biz ID yet! Let's make it!");
-            var stripeState = firebase.functions().httpsCallable('stripeState');
-            stripeState({text: "1234"})
-            .then(function(result){
-              console.log("new state in database, URL returned successfully, redirecting now");
-              var returnedURL = result.data.text;
-              window.location.replace(returnedURL);
-            })
+        if (doc != null && doc != undefined){
+          if (doc.data().stripeBusinessID) {
+            console.log("There is a Biz ID already", doc.data().stripeBusinessID)
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("No biz ID yet! Let's make it!");
+              var stripeState = firebase.functions().httpsCallable('stripeState');
+              stripeState({text: "1234"})
+              .then(function(result){
+                console.log("new state in database, URL returned successfully, redirecting now");
+                var returnedURL = result.data.text;
+                window.location.replace(returnedURL);
+              })
+          }
         }
     }).catch(function(error) {
         console.log("Error getting document:", error);
