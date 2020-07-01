eeebc627e73531caa097d3cb5e177213b6ebd7cc
Jah <kennedye2@vcu.edu>
2020-07-01 16:40:38 -0500

    added the populateMarket() function which references a field called status that doesn't yet exist

--- public/app.js
+++ public/app.js
@@ -49,8 +49,18 @@ function checkForUserPersistence(){
   }
 }
 
-function testThing(){
-  console.log("test thing");
+function populateMarket(){
+  db.collection("businesses").where("status", "==", "doingBusiness")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
 }
 
 // ### Called when user logs in: formats login button to say their ID
