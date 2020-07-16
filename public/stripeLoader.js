787b00e96e923389733310e74708cf6b32e51604
Jah <kennedye2@vcu.edu>
2020-07-15 23:10:51 -0500

    created stripeLoader.js file to load http on local and https on production

---
+++ public/stripeLoader.js
@@ -0,0 +1,14 @@
+if (window.location.hostname === "localhost") {
+
+    var scriptPath = "http://js.stripe.com/v3/";
+
+} else {
+
+    var scriptPath = "https://js.stripe.com/v3/";
+    
+}
+
+var ele = document.createElement("script");
+ele.setAttribute("src", scriptPath);
+document.head.appendChild(ele);
+
