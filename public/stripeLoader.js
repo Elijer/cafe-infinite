7c941e5492890ca2e807a6293acb711b64f94c9f
Jah <kennedye2@vcu.edu>
2020-07-17 10:20:39 -0500

    added stripeloader.js file to load an http or https file appropriately depending on local or production environment

--- public/stripeLoader.js
+++ public/stripeLoader.js
@@ -1,11 +1,7 @@
 if (window.location.hostname === "localhost") {
-
     var scriptPath = "http://js.stripe.com/v3/";
-
 } else {
-
     var scriptPath = "https://js.stripe.com/v3/";
-    
 }
 
 var ele = document.createElement("script");
