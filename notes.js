48e57f4601f300e344356cfbd4755e40869ec9f9
Jah <kennedye2@vcu.edu>
2020-07-18 16:38:19 -0500

    added a Big To-Do to my notes to keep on course

--- notes.js
+++ notes.js
@@ -1,5 +1,9 @@
 /*
 
+Big To-DO
+1. Learn how to do database migration stuff with firestore so I have the tools to back my shit up
+2. Address firestore db rules
+3. Do Unit testing and epxlore possibly using 'Jest' (there's a fireship video on it)
 
 
 
